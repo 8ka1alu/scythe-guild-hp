c3700934fc501b686f9e4e4551cfd5028323b9a3
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 22:04:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const ham = document.getElementById('ham');
-const menu_wrapper = document.getElementById('menu_wrapper');
-ham.addEventListener('click', function() {
-	ham.classList.toggle('clicked');
-	menu_wrapper.classList.toggle('clicked');
-});
+$(function () {
+	
+	$('.zdo_drawer_button').click(function () {
+		$(this).toggleClass('active');
+		$('.zdo_drawer_bg').fadeToggle();
+		$('nav').toggleClass('open');
+	})
+	$('.zdo_drawer_bg').click(function () {
+		$(this).fadeOut();
+		$('.zdo_drawer_button').removeClass('active');
+		$('nav').removeClass('open');
+	});
+})
