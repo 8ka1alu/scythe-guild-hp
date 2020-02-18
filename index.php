1f92a093d4c53bb746370a4bb9a055302d25d2a2
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 17:39:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
 header('Location: /index.html');
-header('Location: /about.html');
-header('Location: /access.html');
-header('Location: /contact.html');
-header('Location: /menu.html');
 
 ?>
