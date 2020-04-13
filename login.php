f4d9ac243be935dca1164c6b6d15a72e33c1640f
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-13 21:23:36 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-require_once('./database.php'); // データベースアクセスファイル読み込み
-require_once('./auth.php'); // ログイン認証ファイル読み込み
+require_once('/database.php'); // データベースアクセスファイル読み込み
+require_once('/auth.php'); // ログイン認証ファイル読み込み
 $errorMessage = ""; // エラーメッセージ初期化
 // ログイン処理
 if ($_POST['mode']=="login") {
   if(!empty($_POST['form']['email']) && !empty($_POST['form']['password'])){
     if ($account=login($_POST['form']['email'], $_POST['form']['password'])){
       $_SESSION['account'] = $account;
-      header("Location: ./login.php");
+      header("Location: /login.php");
     // ログイン失敗時の表示
     } else {
       $errorMessage = "ログインに失敗しました。";
